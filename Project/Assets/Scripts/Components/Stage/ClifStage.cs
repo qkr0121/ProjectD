82ea29e23968d54f51efaed21f70cc9d7796ed14
Lyeol2 <js02232@naver.com>
2022-03-28 19:54:05 +0900

    2022.03.28 class

--- Project/Assets/Scripts/Components/Stage/ClifStage.cs
+++ Project/Assets/Scripts/Components/Stage/ClifStage.cs
@@ -14,20 +14,27 @@ public class ClifStage : Stage
     [Header("등장할 다리")]
     [SerializeField] private GameObject _Bridge;
 
-    [Header("벽 Material")]
-    [SerializeField] private Material _WallMaterial;
+    // 사라질 벽 Material
+    private Material _WallMaterial;
 
-    [Header("레버 Material")]
-    [SerializeField] private Material _LeverMaterial;
+    // 레버 Materail
+    private Material _LeverMaterial;
 
-    [Header("다리 Material")]
-    [SerializeField] private Material _BridgeMaterial;
+    // 다리 Material
+    private Material _BridgeMaterial;
 
     // material 알파 값
     private float alpa = 1.0f;
 
     // Material을 변경함을 나타냅니다.
     private bool isChangeMaterial;
+
+    private void Start()
+    {
+        _WallMaterial = _WallToDisappear.GetComponent<MeshRenderer>().material;
+        _LeverMaterial = _LeverToDisappear.GetComponentsInChildren<MeshRenderer>()[1].material;
+        _BridgeMaterial = _Bridge.GetComponent<MeshRenderer>().material;
+    }
 
     private void Update()
     {
@@ -37,13 +44,7 @@ public class ClifStage : Stage
         }
     }
 
-    private void OnDisable()
-    {
-        _WallMaterial.color = new Color(_WallMaterial.color.r, _WallMaterial.color.g, _WallMaterial.color.b, 1);
-        _LeverMaterial.color = new Color(_LeverMaterial.color.r, _LeverMaterial.color.g, _LeverMaterial.color.b, 1);
-        _BridgeMaterial.color = new Color(_BridgeMaterial.color.r, _BridgeMaterial.color.g, _BridgeMaterial.color.b, 0);
-    }
-
+    // 스테이지가 끝나면 실행합니다(NPC와 마지막 대사후 실행)
     public override void FinishStage()
     {
         isChangeMaterial = true;
@@ -57,6 +58,7 @@ public class ClifStage : Stage
     // Material 알파값을 변경합니다.
     private void ChangeMaterial()
     {
+        // 알파값이 0이하면 실행하지 않습니다.
         if(alpa <= 0.0f)
         {
             isChangeMaterial = false;
